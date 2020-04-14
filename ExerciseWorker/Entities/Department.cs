89e309b524bd08d34d21668cf2f3b65b151ac228
Igor Gaspar <igor.elvis@hotmail.com>
2020-04-14 16:51:17 -0300

    Initial implementation

---
+++ ExerciseWorker/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿namespace ExerciseWorker.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
